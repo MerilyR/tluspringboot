ee04396323d1d63d94ec0381f8e4ca9cd0a07fdc
MerilyR <merily.r@gmail.com>
2018-03-08 15:45:51 +0200

    links
    Links to Heroku

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -10,21 +10,25 @@ public class Rakendus {
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
         return "Tere hommikust!";
+        //https://tluspringboot.herokuapp.com/algus
     }
     
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
+        //https://tluspringboot.herokuapp.com/tervitus?eesnimi=Merily
     }
     
     @RequestMapping("/korrutus")
     int korrutamine (int a, int b){
         return a*b;
+        //https://tluspringboot.herokuapp.com/korrutus?a=2&b=3
     }
     
     @RequestMapping("/division")
     String divide (int a, int b){
         return a+" divided by "+b+" equals "+a/b;
+        //https://tluspringboot.herokuapp.com/division?a=2&b=3
     }
     
     @RequestMapping("/sum")
@@ -36,6 +40,7 @@ public class Rakendus {
 //        return sum;
         return java.util.Arrays.stream(numbers.split(",")).
                 mapToInt(s -> Integer.parseInt(s.trim())).sum();
+        //https://tluspringboot.herokuapp.com/sum?numbers=1,%202,%203,%204
     }
         
     public static void main(String[] args) {
